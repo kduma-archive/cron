628a12343db0fa9cbc0d0bf709796b1a0b840187
kduma <kduma@users.noreply.github.com>
2014-11-25 03:52:53 +0100

    Added continous operation and time limits

--- Console/CronCommand.php
+++ Console/CronCommand.php
@@ -3,6 +3,7 @@
 use Illuminate\Console\Command;
 use October\Rain\Cron\Models\Job;
 use October\Rain\Cron\CronJob;
+use Symfony\Component\Console\Input\InputOption;
 
 class CronCommand extends Command
 {
@@ -28,10 +29,26 @@ class CronCommand extends Command
      */
     public function fire()
     {
-        if ($job = Job::isAvailable()->first()) {
-            $cronJob = new CronJob($this->laravel, $job);
-            $cronJob->fire();
-        }
-    }
+	    $runs = 0;
+	    $time = microtime(true);
+	    while ($job = Job::isAvailable()->first()) {
+		    $cronJob = new CronJob($this->laravel, $job);
+		    $cronJob->fire();
 
+		    if(++$runs >= $this->option('runlimit'))break;
+		    if((microtime(true) - $time) > $this->option('timelimit'))break;
+	    }
+    }
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('timelimit', 't', InputOption::VALUE_OPTIONAL, 'A time limited to run all commands in seconds.', 50),
+			array('runlimit', 'r', InputOption::VALUE_OPTIONAL, 'Maximum runs.',100),
+		);
+	}
 }
